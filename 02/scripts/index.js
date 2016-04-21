c7ea95132f40f2b258442940382f7d67cd1dc7f1
Esperanza <hola@mi-mina.com>
2016-04-21 21:23:59 +0200

    refactorizando exp 02

--- 02/scripts/index.js
+++ 02/scripts/index.js
@@ -374,58 +374,56 @@ module.exports = function(arr, obj){
 },{}],6:[function(require,module,exports){
 var h = require('hyperscript');
 
-document.body.appendChild(
-  h('section.page-header',
-    h('h1.project-name', "Experimento #2"),
-    h('h2.project-tagline', 'Generar html utilizando virtual DOM'),
-    h("a.button", {href: "https://github.com/mi-mina/experimento"}, "View on GitHub"),
-    h("a.button", {href: "https://github.com/mi-mina/experimento/archive/master.zip"}, "Download .zip"))
-  );
+function renderPage(json){
+  return h('body', [h('section.page-header',
+          h('h1.project-name', "Experimento #2"),
+          h('h2.project-tagline', 'Generar html dinámicamente con hyperscript y Fetch API'),
+          h("a.button", {href: "https://github.com/mi-mina/experimento"}, "View on GitHub"),
+          h("a.button", {href: "https://github.com/mi-mina/experimento/archive/master.zip"}, "Download .zip")),
+          h("section#main.main-content",
+            h("div",
+              h("button.toogle.btn.btn-info.btn-lg", "Show/Hide Info")),
+            h("article#albums",
+              Object.keys(json).map(
+                function(album){
+                  return h("div.media",
+                  h("div.media-left.media-middle",
+                  h("img#img.media-object.img-thumbnail", {src: "public/images/"+json[album].nombreArchivo+".jpg"})),
+                  h("div.media-body",
+                  h("h2.media-heading", json[album].titulo),
+                  h("ul", {style: {"list-style-type": "none", "padding": 0}},
+                  h("li", h("b", "Album: "), json[album].album),
+                  h("li", h("b", "Artista: "), json[album].artista),
+                  h("li", h("b", "Lugar: "), json[album].lugar),
+                  h("li", h("b", "Fecha: "), json[album].fecha))));
+                }
+              )
+          ))])
+       ;
+};
 
-var noDetails = true;
+var toogle;
+var noDetails = false;
 
-document.body.appendChild(
-  h("section#main.main-content",
-    h("div",
-      h("button.toogle.btn.btn-info.btn-lg", "Show/Hide Info")),
-    h("article#albums")));
-
-document.body.onload = function (){
-  console.log("cargando body");
-    fetch('public/info.json')
+fetch('public/info.json')
       .then(function(response) {
         return response.json();
-      }).then(function(json) {
-        for(var i = 0; i < json.length; i++){
-          document.getElementById('albums').appendChild(
-            h("div.media",
-              h("div.media-left.media-middle",
-                h("img#img.media-object.img-thumbnail"+i, {src: "public/images/"+json[i].nombreArchivo+".jpg"})),
-              h("div.media-body",
-                h("h2.media-heading", json[i].titulo),
-                h("ul", {style: {"list-style-type": "none", "padding": 0}, hidden: noDetails},
-                  h("li", h("b", "Album: "), json[i].album),
-                  h("li", h("b", "Artista: "), json[i].artista),
-                  h("li", h("b", "Lugar: "), json[i].lugar),
-                  h("li", h("b", "Fecha: "), json[i].fecha))))
-          )
-        }
+      }).then(function(data){
+        console.log(data);
+        document.body.innerHTML = renderPage(data).outerHTML;
+        toogle = document.querySelector('.toogle');
+        toogle.onclick = function () {
+          noDetails = !noDetails;
+          var items = document.getElementsByTagName("ul");
+
+          for(var i = 0; i < items.length; i++){
+            if (noDetails) {
+              items[i].setAttribute("hidden", true);
+            }else{
+              items[i].removeAttribute("hidden");
+            };
+          };
+        };
       });
-};
-
-var toogle = document.querySelector('.toogle');
-
-toogle.onclick = function () {
-  noDetails = !noDetails;
-  var items = document.getElementsByTagName("ul");
-
-  for(var i = 0; i < items.length; i++){
-    if (noDetails) {
-      items[i].setAttribute("hidden", true);
-    }else{
-      items[i].removeAttribute("hidden");
-    };
-  };
-};
 
 },{"hyperscript":2}]},{},[6]);
