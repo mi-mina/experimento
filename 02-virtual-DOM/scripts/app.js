4440c1ed0f5ce9063391b932a4112b05d3dbfdc0
Esperanza <hola@mi-mina.com>
2016-04-21 14:27:47 +0200

    toogle info

--- 02-virtual-DOM/scripts/app.js
+++ 02-virtual-DOM/scripts/app.js
@@ -2,31 +2,54 @@ var h = require('hyperscript');
 
 document.body.appendChild(
   h('section.page-header',
-    h('h1.project-name', "Experimento #2"),
+    h('h1.project-name', "Experimento #2.1"),
     h('h2.project-tagline', 'Generar html utilizando virtual DOM'),
-    h("a.btn", {href: "https://github.com/mi-mina/experimento"}, "View on GitHub"),
-    h("a.btn", {href: "https://github.com/mi-mina/experimento/archive/master.zip"}, "Download .zip"))
+    h("a.button", {href: "https://github.com/mi-mina/experimento"}, "View on GitHub"),
+    h("a.button", {href: "https://github.com/mi-mina/experimento/archive/master.zip"}, "Download .zip"))
   );
 
-document.body.appendChild(
-  h("section#main.main-content")
-);
+var noDetails = true;
 
-fetch('public/info.json')
-  .then(function(response) {
-    return response.json();
-  }).then(function(json) {
-    for(var i = 0; i < json.length; i++){
-      document.getElementById('main').appendChild(
-        h("div.media",
-          h("div.media-left.media-middle",
-            h("img#img.media-object.img-thumbnail"+i, {src: "public/images/"+json[i].nombreArchivo+".jpg"})),
-          h("div.media-body",
-            h("h2.media-heading", json[i].titulo),
-            h("p", "Album: "+json[i].album),
-            h("p", "Artista: "+json[i].artista),
-            h("p", "Lugar: "+json[i].lugar),
-            h("p", "Fecha: "+json[i].fecha)))
-      )
-    }
-  });
+document.body.appendChild(
+  h("section#main.main-content",
+    h("div",
+      h("button.toogle.btn.btn-info.btn-lg", "Show/Hide Info")),
+    h("article#albums")));
+
+document.body.onload = function (){
+  console.log("cargando body");
+    fetch('public/info.json')
+      .then(function(response) {
+        return response.json();
+      }).then(function(json) {
+        for(var i = 0; i < json.length; i++){
+          document.getElementById('albums').appendChild(
+            h("div.media",
+              h("div.media-left.media-middle",
+                h("img#img.media-object.img-thumbnail"+i, {src: "public/images/"+json[i].nombreArchivo+".jpg"})),
+              h("div.media-body",
+                h("h2.media-heading", json[i].titulo),
+                h("ul", {style: {"list-style-type": "none", "padding": 0}, hidden: noDetails},
+                  h("li", h("b", "Album: "), json[i].album),
+                  h("li", h("b", "Artista: "), json[i].artista),
+                  h("li", h("b", "Lugar: "), json[i].lugar),
+                  h("li", h("b", "Fecha: "), json[i].fecha))))
+          )
+        }
+      });
+};
+
+var toogle = document.querySelector('.toogle');
+
+toogle.onclick = function () {
+  noDetails = !noDetails;
+  var items = document.getElementsByTagName("ul");
+
+  for(var i = 0; i < items.length; i++){
+    if (noDetails) {
+      items[i].setAttribute("hidden", true);
+    }else{
+      items[i].removeAttribute("hidden");
+    };
+  };
+};
